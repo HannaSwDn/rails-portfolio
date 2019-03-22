300d274ee65df90679135e2a8eb06b3f2fa5804f
hanna_nyman <hannanyman.sk@gmail.com>
2019-03-22 08:45:48 +0100

    Adds basic_steps and assertion_steps files

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,11 @@
+Given("I visit the site") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click on {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I fill in the contact form") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
